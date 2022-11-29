a4b4cb1ee746c0b594e7eff7b4da219e22cd9adf
daniela rocha <rocha072005@gmail.com>
2022-11-28 19:54:58 -0500

    Agrega vista pagina de agendamiento y modifica imagen de pag principal

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const router=express.Router()
+const app=express()
+
+
+
+module.exports=router
+
+var ractive = new Ractive({
+    el: '#container',
+    template: '#template',
+    data: {}
+  });
+  
