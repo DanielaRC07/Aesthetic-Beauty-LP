3e43a0f1b2661b12ba2759327431ab669a579a37
daniela rocha <rocha072005@gmail.com>
2022-12-05 20:21:10 -0500

    Agrega vista consulta y visualizacion cita

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,29 @@
 const express=require('express')
 const router=express.Router()
-const app=express()
 
+router.get('/',(req,res)=>{
+	res.render('index')
+})
+
+router.get('/Home',(req,res)=>{
+	res.render('index')
+})
+
+router.get('/Registrar',(req,res)=>{
+	res.render('registro')
+})
+
+router.get('/MySide',(req,res)=>{
+	res.render('login')
+})
+
+router.get('/Ver',(req,res)=>{
+	res.render('consulta')
+})
+
+router.get('/Update',(req,res)=>{
+	res.render('Actualizar')
+})
 
 
 module.exports=router
-
-var ractive = new Ractive({
-    el: '#container',
-    template: '#template',
-    data: {}
-  });
-  
