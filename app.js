c4d651a7af6af9ab20d01be82f30d7dd3e1e9534
daniela rocha <rocha072005@gmail.com>
2023-02-05 22:24:10 -0500

    Agrega dependencias express

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-const rutas = require("./routes");
+const routes = require("./routes");
 
 app.set("port", 3001);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", rutas);
+app.use("/", routes);
 app.use("/public", express.static("public"));
 app.use(cors());
 
